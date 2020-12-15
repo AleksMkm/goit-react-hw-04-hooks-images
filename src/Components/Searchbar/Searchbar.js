4004701dd681309cd31444d2085047206b6509ea
Aleksandr Mukomelov <mukomelov.aleksandr@gmail.com>
2020-12-15 20:27:34 +0200

    refactor: code refactored from classes to hooks (Searchbar Component)

--- src/Components/Searchbar/Searchbar.js
+++ src/Components/Searchbar/Searchbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useRef } from 'react';
 import { FiSearch } from 'react-icons/fi';
 import { IconContext } from 'react-icons';
 import { toast } from 'react-toastify';
@@ -6,54 +6,42 @@ import PropTypes from 'prop-types';
 
 import s from './Searchbar.module.css';
 
-class Searchbar extends Component {
-  constructor(props) {
-    super(props);
-    this.btn = React.createRef();
-  }
+function Searchbar({ getSearchQuery }) {
+  const [value, setValue] = useState('');
 
-  state = {
-    value: '',
-  };
+  const btn = useRef(null);
 
-  handleInput = e => {
-    this.setState({ value: e.target.value });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    const query = this.state.value.toLowerCase().trim();
+    const query = value.toLowerCase().trim();
     if (query === '') {
       toast.error('Please enter a query');
-      this.btn.current.blur();
+      btn.current.blur();
       return;
     }
-    this.props.getSearchQuery(query);
-    this.setState({ value: '' });
-    this.btn.current.blur();
+    getSearchQuery(query);
+    setValue('');
+    btn.current.blur();
   };
 
-  render() {
-    return (
-      <header className={s.searchbar}>
-        <form className={s.form} onSubmit={this.handleSubmit}>
-          <button type="submit" className={s.btn} ref={this.btn}>
-            <IconContext.Provider value={{ size: '16px' }}>
-              <FiSearch />
-            </IconContext.Provider>
-          </button>
-
-          <input
-            className={s.input}
-            type="text"
-            value={this.state.value}
-            placeholder="Search images and photos"
-            onChange={this.handleInput}
-          />
-        </form>
-      </header>
-    );
-  }
+  return (
+    <header className={s.searchbar}>
+      <form className={s.form} onSubmit={handleSubmit}>
+        <button type="submit" className={s.btn} ref={btn}>
+          <IconContext.Provider value={{ size: '16px' }}>
+            <FiSearch />
+          </IconContext.Provider>
+        </button>
+        <input
+          className={s.input}
+          type="text"
+          value={value}
+          placeholder="Search images and photos"
+          onChange={e => setValue(e.target.value)}
+        />
+      </form>
+    </header>
+  );
 }
 
 export default Searchbar;
